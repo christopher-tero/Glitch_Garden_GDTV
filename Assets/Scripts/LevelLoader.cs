a76af06b1d66cf425cf99f360b2ba95340ca1498
Christopher Tero <christopher.tero@gmail.com>
2020-01-01 00:16:01 -0600

    added splash and start screens

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int waitTime = 3;
+    int currentSceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(waitTime);
+        LoadNextScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
